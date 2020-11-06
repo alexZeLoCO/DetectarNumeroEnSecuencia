e10abffe4207508474589963e7bdb722e7c2a2e7
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-11-06 10:48:00 +0100

    Segundo Commit

--- DetectarNúmeroEnSecuencia/src/DetectarNumeroEnSecuencia.java
+++ DetectarNúmeroEnSecuencia/src/DetectarNumeroEnSecuencia.java
@@ -8,17 +8,16 @@ public class DetectarNumeroEnSecuencia {
 		
 		do {
 			n = teclado.nextInt();		//PIDE VALOR
-			
-			if (n%5==0) {		//SI HAY MÚLTIPLO DE 5
-				System.out.println("La secuencia contiene un múltiplo de 5.");		//OUTPUT
-			}
-			if (n%2==1) {		//SI HAY UN NÚMERO IMPAR
-				System.out.println("La secuencia contiene un número impar.");		//OUTPUT
-			}
-			if (n<0 && n%2==0 && n%5==1) {		//SI NO HAY NI IMPAR NI MÚLTIPLO DE 5
-				System.out.print("La secuencia no contiene ningún valor impar o múltiplo de 5.");		//OUTPUT
-			}
 		} while (n>=0 && n%2==0 && n%5!=0); 		//CONDICIÓN
-	
+		
+		if (n%5==0 && n>0) {		//SI HAY MÚLTIPLO DE 5
+			System.out.println("La secuencia contiene un múltiplo de 5.");		//OUTPUT
+		}
+		if (n%2==1 && n>0) {		//SI HAY UN NÚMERO IMPAR
+			System.out.println("La secuencia contiene un número impar.");		//OUTPUT
+		}
+		if (n<0) {		//SI NO HAY NI IMPAR NI MÚLTIPLO DE 5
+			System.out.print("La secuencia no contiene ningún valor impar o múltiplo de 5.");		//OUTPUT
+		}
 	}
 }
